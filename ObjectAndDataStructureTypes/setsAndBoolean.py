e91b2f571c865e957b68aae7924b40e990cac43f
NagarajuSaripally <nagaraju.saripally@gmail.com>
2018-03-14 21:08:47 -0500

    Set and Boolean types, Including None type

---
+++ ObjectAndDataStructureTypes/setsAndBoolean.py
@@ -0,0 +1,34 @@
+'''
+Sets : are unordered collection of unique sets
+'''
+
+my_sets = set()
+
+my_sets.add(1)
+
+print('Unique sets {}'.format(my_sets))
+
+#if we add same number multiple times,  it only takes those repeated values only one time like
+
+my_sets = [1,2,3,4,2,2,1,1,1,3,3,3,4,4]
+
+print('Sets of unorder collection of unique values {}'.format(set(my_sets)))
+
+
+'''
+Boolean : True or False
+
+In python, the type should start with capital letter like above, true returns an syntax error
+
+and the type is : bool
+'''
+
+print('Boolean value {}'.format(1> 2))
+
+'''
+ To add a placeholder variables we can use None like below
+'''
+
+n = None
+
+print(n)
