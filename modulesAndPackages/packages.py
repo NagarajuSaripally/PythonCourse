b6f9d7688838f0a6138cb3ebb0eb4292377bb5ff
NagarajuSaripally <nagaraju.saripally@gmail.com>
2018-04-04 22:16:25 -0500

    Package installation with pypi

---
+++ modulesAndPackages/packages.py
@@ -0,0 +1,35 @@
+"""
+Like NPM (node package manager) in javascript, there is a package manager for python which is called *PYPI*
+
+we can use pip install on command line
+
+By installing Pythpn from python.ord or through the Anaconda distribution that comes with installation of *pip*
+
+*pip* is simple way to download packages at our command line directly from the PyPi repository.
+
+There are packages already created as it is highly popularized
+
+Syntax:
+
+pip install requests
+
+pip install colorama
+
+to use it
+Python, 
+
+from colorama import init
+init()
+
+from colorama import Fore
+
+print(Fore.RED + "Some red text")
+
+--> it print some red text in red color
+
+
+python package for excel:
+
+package name is : Python Excel
+
+"""
