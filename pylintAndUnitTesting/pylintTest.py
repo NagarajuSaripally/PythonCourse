c9e495f0fce938e21c132b2aaa94afefdc6f17ae
NagarajuSaripally <nagaraju.saripally@gmail.com>
2021-06-30 15:01:57 -0500

    pylint and unit test

---
+++ pylintAndUnitTesting/pylintTest.py
@@ -0,0 +1,17 @@
+'''
+ testing tools:
+ pylint: libarary that looks at our code and reports back possible issues
+ : python as a set of style convention rules know as "PEP 8"
+
+ unittest: built-in library allows us to test our own programs and check if we are getting
+ desired outputs
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
---
+++ pylintAndUnitTesting/unitTesting.py
@@ -0,0 +1,9 @@
+'''
+capitalize() - tansforms first letter of first to uppercase - it wont
+               capitalize multiple words
+title() - transforms all the worlds first letter to uppercase
+'''
+
+def cap_text(text):
+  #return text.capitalize()
+  return text.title()
